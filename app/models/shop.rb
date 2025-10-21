c76124ab7816e90ef4b7789d72d0240de495f61b
Madoka Uchino <madoka@MadokanoMacBook-Air.local>
2025-10-21 17:28:24 +0900

    バリデーションエラーメッセージの日本語化を実装

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,7 @@
 class Shop < ApplicationRecord
   belongs_to :user
-  belongs_to :genre
+  belongs_to :genre, optional: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :genre_id, presence: true
 end
