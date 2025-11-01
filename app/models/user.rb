9cba61fcfa952e6a85a4ff6c8673f13422717f4d
Madoka Uchino <madoka@MadokanoMacBook-Air.local>
2025-11-01 15:44:20 +0900

    犬情報の登録、表示機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :favorite_shops, through: :shop_favorites, source: :shop
   has_many :comments, dependent: :destroy
   has_one :dog, dependent: :destroy
+  has_one_attached :profile_image
 
   accepts_nested_attributes_for :dog
   
