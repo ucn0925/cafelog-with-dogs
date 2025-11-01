b91ee379a4ee277012a895bbae9483170dd61cdf
Madoka Uchino <madoka@MadokanoMacBook-Air.local>
2025-11-01 17:36:51 +0900

    他ユーザーのマイページアクセスを制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    if @user != current_user
+      redirect_to root_path, alert: "他のユーザーのページにはアクセスできません。"
+    end
   end
 end
